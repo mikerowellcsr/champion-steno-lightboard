57b21a113d9806ef658b9c72ab8c1499fadac490
stanographer <sakai.stanley@gmail.com>
2018-09-14 18:31:11 -0400

    Login issues

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,7 +3,11 @@ import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCog,
+    faUser,
+    faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons';
 import {
     Container,
     Collapse,
@@ -17,26 +21,50 @@ import {
 import SlidingPane from 'react-sliding-pane';
 import DocumentTitle from 'react-document-title';
 import ConfigSpeakerDeck from './Config/SpeakerDeck';
-import SignOutButton from './SignOut';
+import { auth } from '../firebase';
 
 import * as routes from '../Constants/Routes';
 
 class Navigation extends Component {
     constructor(props) {
         super(props);
+    }
+
+    render() {
+        return(
+            <div>
+                {
+                    this.props.authUser
+                    ? <NavigationAuth />
+                    : <NavigationNonAuth />
+                }
+            </div>
+        );
+    }
+}
+
+class NavigationAuth extends Component {
+    constructor() {
+        super();
 
         this.state = {
             isPaneOpen: false,
             isPaneOpenLeft: false,
             isOpen: false
         };
-
         this.toggle = this.toggle.bind(this);
+        this.toggleLeftPane = this.toggleLeftPane.bind(this);
     }
 
     toggle() {
         this.setState({
-            isOpen: !this.state.isOpen
+            isPaneOpenLeft: !this.state.isPaneOpenLeft
+        });
+    }
+
+    toggleLeftPane() {
+        this.setState({
+            isPaneOpenLeft: true
         });
     }
 
@@ -46,53 +74,53 @@ class Navigation extends Component {
 
     render() {
         library.add(faCog);
-        return(
-            <div>
-                {
-                    this.props.authUser
-                    ? <NavigationAuth state={this.state} />
-                    : <NavigationNonAuth />
-                }
-            </div>
-        );
+        library.add(faUser);
+        library.add(faSignOutAlt);
+
+        return <div>
+            <Navbar color="dark" dark expand="md">
+                <NavbarBrand href="/">Champion Steno Lightboard</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} />
+                <Collapse isOpen={this.state.isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/account">
+                                <FontAwesomeIcon icon="user" />
+                                &nbsp;&nbsp;Account
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="#" onClick={ () => this.setState({ isPaneOpenLeft: true }) }>
+                                <FontAwesomeIcon icon="cog" />
+                                &nbsp;&nbsp;Configure
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink onClick={auth.doSignOut}>
+                                <FontAwesomeIcon icon="sign-out-alt" />
+                                &nbsp;&nbsp;Sign Out
+                            </NavLink>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+            <Container>
+                <DocumentTitle title="Champion Steno Lightboard" />
+                <div ref={ref => this.el = ref}>
+                    <SlidingPane
+                        isOpen={ this.state.isPaneOpenLeft }
+                        title="Preferences"
+                        from="left"
+                        width="80%"
+                        onRequestClose={ () => this.setState({ isPaneOpenLeft: false }) }>
+                        <h2>Speaker Deck</h2>
+                        <ConfigSpeakerDeck />
+                    </SlidingPane>
+                </div>
+            </Container>
+        </div>;
     }
 }
-
-
-const NavigationAuth = ({ state }) =>
-    <div>
-        <Navbar color="dark" dark expand="md">
-            <NavbarBrand href="/">Champion Steno Lightboard</NavbarBrand>
-            <NavbarToggler onClick={this.props.toggle} />
-            <Collapse isOpen={this.props.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                    <NavItem>
-                        <NavLink href="/account/">Account</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="#" onClick={ () => this.setState({ isPaneOpenLeft: true }) }>
-                            <FontAwesomeIcon icon="cog" /> Configure
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </Navbar>
-        <Container>
-            <DocumentTitle title="Champion Steno Lightboard" />
-            <div ref={ref => this.el = ref}>
-                <SlidingPane
-                    isOpen={ this.state.isPaneOpenLeft }
-                    title="Preferences"
-                    from="left"
-                    width="80%"
-                    onRequestClose={ () => this.setState({ isPaneOpenLeft: false }) }>
-                    <h2>Speaker Deck</h2>
-                    <ConfigSpeakerDeck />
-                </SlidingPane>
-            </div>
-        </Container>
-    </div>;
-
 const NavigationNonAuth = () =>
     <ul>
         <li><Link to={routes.LANDING}>Landing</Link></li>
--- src/Components/SignOut.js
+++ src/Components/SignOut.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { auth } from '../firebase';
 
 const SignOutButton = () =>
