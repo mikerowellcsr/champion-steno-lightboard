a6983bc0c0cbcd8d23f5ab275e46549bad2166c4
stanographer <sakai.stanley@gmail.com>
2018-10-26 19:42:28 -0400

    Finally got file uploads working.

--- client/src/components/elements/SpeakerDeckConfigComponent.js
+++ client/src/components/elements/SpeakerDeckConfigComponent.js
@@ -2,101 +2,121 @@ import React, { Component } from 'react';
 import {
     Table
 } from 'reactstrap';
-import Speaker0Dropzone from './dropzones/Speaker0';
-import './filepondcustom.css';
+import SpeakerUploader from './SpeakerUploader';
+import { speakerdeck } from '../../firebase';
 
-class SpeakerPhotoUploadComponent extends Component {
+class SpeakerDeckConfigComponent extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-             activeSpeaker: null
+            activespeaker: null
         };
+    }
 
+    componentDidMount() {
+        const { dispatch } = this.props;
+
+        speakerdeck.fetchSpeakerPhotos().then(snapshot => {
+            dispatch(snapshot.val());
+        });
     }
 
     render() {
+        const { dispatch, photos } = this.props;
+        console.log(photos);
         const active = {
             color: 'primary',
             class: 'inverse'
         };
+
         const { activeSpeaker } = this.state;
 
         return (
             <div className="configure-pane">
-                <h3>Click a frame or drag an image file over it to upload photo.</h3>
+                <h2>Preview</h2>
                 <Table
                     borderless
                     responsive>
                     <thead>
-                        <tr>
-                            <th>
-                                Defense
-                            </th>
-                            <th>
-                                Witness
-                            </th>
-                            <th>
-                                The Court
-                            </th>
-                            <th>
-                                Plaintiff
-                            </th>
-                            <th>
-                                Defense 2
-                            </th>
-                        </tr>
+                    <tr>
+                        <th>
+                            Defense
+                        </th>
+                        <th>
+                            Witness
+                        </th>
+                        <th>
+                            The Court
+                        </th>
+                        <th>
+                            Plaintiff
+                        </th>
+                        <th>
+                            Defense 2
+                        </th>
+                    </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>
-                                <img width="100%"
-                                         id="speaker0"
-                                         src="/images/speaker-placeholder.png"
-                                         alt="Card image cap"/>
-                            </td>
-                            <td>
-                                <img width="100%"
-                                         id="speaker0"
-                                         src="/images/speaker-placeholder.png"
-                                         alt="Card image cap"/>
-                            </td>
-                            <td>
-                                <img width="100%"
-                                         id="speaker0"
-                                         src="/images/speaker-placeholder.png"
-                                         alt="Card image cap"/>
-                            </td>
-                            <td>
-                                <img width="100%"
-                                         id="speaker0"
-                                         src="/images/speaker-placeholder.png"
-                                         alt="Card image cap"/>
-                            </td>
-                            <td>
-                                <img width="100%"
-                                         id="speaker0"
-                                         src="/images/speaker-placeholder.png"
-                                         alt="Card image cap"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <Speaker0Dropzone />
-                            </td>
-                            <td>
-                                <Speaker0Dropzone />
-                            </td>
-                            <td>
-                                <Speaker0Dropzone />
-                            </td>
-                            <td>
-                                <Speaker0Dropzone />
-                            </td>
-                            <td>
-                                <Speaker0Dropzone />
-                            </td>
-                        </tr>
+                    <tr>
+                        <td>
+                            <img width="100%"
+                                 id="speaker0"
+                                 src={photos.hasOwnProperty(0) ? photos[0].imageUrl : "/images/speaker-placeholder.png"}
+                                 alt="Card image cap" />
+                        </td>
+                        <td>
+                            <img width="100%"
+                                 id="speaker0"
+                                 src="/images/speaker-placeholder.png"
+                                 alt="Card image cap" />
+                        </td>
+                        <td>
+                            <img width="100%"
+                                 id="speaker0"
+                                 src="/images/speaker-placeholder.png"
+                                 alt="Card image cap" />
+                        </td>
+                        <td>
+                            <img width="100%"
+                                 id="speaker0"
+                                 src="/images/speaker-placeholder.png"
+                                 alt="Card image cap" />
+                        </td>
+                        <td>
+                            <img width="100%"
+                                 id="speaker0"
+                                 src="/images/speaker-placeholder.png"
+                                 alt="Card image cap" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <SpeakerUploader
+                                dispatch={dispatch}
+                                speakerNumber={ 0 } />
+                        </td>
+                        <td>
+                            <SpeakerUploader
+                                dispatch={dispatch}
+                                speakerNumber={ 1 } />
+                        </td>
+                        <td>
+                            <SpeakerUploader
+                                dispatch={dispatch}
+                                speakerNumber={ 2 } />
+                        </td>
+                        <td>
+                            <SpeakerUploader
+                                dispatch={dispatch}
+                                speakerNumber={ 3 } />
+                        </td>
+                        <td>
+                            <SpeakerUploader
+                                dispatch={dispatch}
+                                speakerNumber={ 4 } />
+                        </td>
+                    </tr>
                     </tbody>
                 </Table>
             </div>
@@ -104,4 +124,4 @@ class SpeakerPhotoUploadComponent extends Component {
     }
 }
 
-export default SpeakerPhotoUploadComponent;
+export default SpeakerDeckConfigComponent;
---
+++ client/src/components/elements/SpeakerUploader.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import { FilePond, registerPlugin } from 'react-filepond';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginImageResize from 'filepond-plugin-image-resize';
+import 'filepond/dist/filepond.min.css';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import { db, storage } from '../../firebase/firebase';
+
+registerPlugin(
+    FilePondPluginImageResize,
+    FilePondPluginImagePreview,
+    FilePondPluginFileValidateType
+);
+
+class SpeakerUploader extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            files: [],
+            image: null,
+            imageUrl: ''
+        };
+
+        this.handleProcessing = this.handleProcessing.bind(this);
+    }
+
+    handleInit = () => {
+        console.log(`FilePond has initialized.`);
+    };
+
+    handleProcessing = (fieldName, file, metadata, load, error, progress, abort) => {
+
+        const fileUpload = file;
+        const { dispatch, speakerNumber } = this.props;
+        const dbRef = db.ref(`/`);
+        const storageRef = storage.ref(`images/${file.name}`);
+        const uploadTask = storageRef.put(fileUpload);
+
+        progress(true, 0, 1024);
+
+        uploadTask.on('state_changed',
+
+            // Progress function.
+            snapshot => {
+                progress(true, snapshot.bytesTransferred, snapshot.totalBytes);
+            },
+
+            // Error function.
+            e => {
+                error(`Upload error: ${e}`);
+            },
+
+            // Complete function.
+            () => {
+                load(uploadTask.snapshot.ref.fullPath);
+
+                storage.ref(`images`).child(file.name).getDownloadURL()
+                    .then(url => {
+                        console.log(url);
+
+                        // Checks to see if an image is already assigned to a speaker.
+                        // If it is, then replace the image. If not, push up a new image ref.
+                        dbRef.child(`speakerPhotos`).once(`value`, snapshot => {
+                            if (!snapshot.val() || !snapshot.val().hasOwnProperty(speakerNumber)) {
+                                console.log('nothing found');
+                                dbRef.child(`speakerPhotos`).child(speakerNumber).set({
+                                    index: speakerNumber,
+                                    imageUrl: url
+                                });
+                            } else {
+                                dbRef.child(`speakerPhotos`).update({
+                                    [speakerNumber]: {
+                                        index: speakerNumber,
+                                        imageUrl: url
+                                    }
+                                });
+                            }
+                            dispatch(snapshot.val());
+                        })
+                    });
+            });
+
+        return {
+            abort: () => {
+                // This function is entered if the user has tapped the cancel button.
+                uploadTask.cancel();
+
+                // Let FilePond know the request has been cancelled.
+                abort();
+            }
+        };
+    };
+
+    render() {
+        return (
+            <div>
+                <FilePond
+                    acceptedFileTypes={ ['image/gif', 'image/jpeg', 'image/jpg', 'image/png'] }
+                    fileValidateTypeLabelExpectedTypes='Allowed: .jpg, .png, .gif'
+                    imageResizeTargetWidth={ 300 }
+                    imageResizeTargetHeight={ 300 }
+                    imageResizeMode='cover'
+                    labelFileTypeNotAllowed='File type invalid'
+                    labelIdle='Drag photo or <span class="filepond--label-action">browse</span>'
+                    oninit={ this.handleInit }
+                    allowMultiple={ false }
+                    server={ { process: this.handleProcessing } }
+                    ref={ ref => this.pond = ref }
+                >
+                </FilePond>
+            </div>
+        );
+    }
+}
+
+export default SpeakerUploader;
