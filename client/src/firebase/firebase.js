548691a7232cddf7d298a1b2705fad010bf86bf8
stanographer <sakai.stanley@gmail.com>
2018-11-04 15:12:17 -0500

    Added env variables for API secrets.

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,37 @@
+// Firebase settings
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+// Contains API secrets.
+// Be sure to update with your Firebase API values.
+import devConfig from './devconfig';
+
+const prodConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
+};
+
+const config = process.env.REACT_APP_ENV === `dev`
+    ? devConfig
+    : prodConfig;
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+const auth = firebase.auth();
+const db = firebase.database();
+const storage = firebase.storage();
+
+export {
+    auth,
+    db,
+    storage
+};
