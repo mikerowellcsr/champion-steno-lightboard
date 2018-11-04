95371b7bf3af7c232986337874ce548b7bcaea55
stanographer <sakai.stanley@gmail.com>
2018-11-04 14:23:49 -0500

    Fixing app.js.

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const cors = require('cors');
-const cowsay = require('cowsay');
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const WebSocket = require('ws').Server;
+const cors = require(`cors`);
+const cowsay = require(`cowsay`);
+const express = require(`express`);
+const http = require(`http`);
+const path = require(`path`);
+const WebSocket = require(`ws`).Server;
 const app = express();
 const httpServer = http.createServer(app);
 let PORT = process.env.PORT || 8000;
-
-
 
 // Create new WebSockets connection.
 const wss = new WebSocket({
@@ -16,24 +14,25 @@ const wss = new WebSocket({
 });
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: `http://localhost:3000`,
+    // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200
 };
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, `client/build`)));
 app.use(cors());
 
 
 // Global path.
-app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+app.get(`*`, (request, response) => {
+    response.sendFile(path.join(__dirname, `client/build`, `index.html`));
 });
 
 // Serve our base route that returns a Hello World cow
-app.get('/api/cow/', cors(corsOptions), async(req, res, next) => {
+app.get(`/api/cow/`, cors(corsOptions), async(req, res, next) => {
     try {
-        const moo = cowsay.say({ text: 'Hello World!' });
+        const moo = cowsay.say({ text: `Hello World!` });
         res.json({ moo });
     } catch (err) {
         next(err);
@@ -71,66 +70,66 @@ const broadcast = (data, ws) => {
     });
 };
 
-wss.on('connection', ws => {
+wss.on(`connection`, ws => {
     let userId;
 
-    ws.on('message', message => {
+    ws.on(`message`, message => {
         const data = JSON.parse(message);
         console.log(data);
         switch (data.type) {
-            case 'USER_LOGGED_ON':
-
+            case `USER_LOGGED_ON`:
                 userId = data.id;
 
-                console.log('username: ' + data.username);
+                // Checks to see if the user exists before adding to the list.
+                console.log(`username:  ${data.username}`);
                 if (!users.checkIfUserExists(data.username)) {
                     users.addUser({
                         id: data.id,
-                        username: data.username ? data.username.trim() : '',
+                        username: data.username ? data.username.trim() : ``,
                         logOnTime: data.logOnTime
                     });
                 }
 
                 ws.send(JSON.stringify({
-                    type: 'LIST_USERS',
+                    type: `LIST_USERS`,
                     users: users.listUsers()
                 }));
 
+                // Broadcast out the updated list of users.
                 broadcast({
-                    type: 'LIST_USERS',
+                    type: `LIST_USERS`,
                     users: users.listUsers()
                 }, ws);
 
-                console.log('\n users in ' + JSON.stringify(users.listUsers()));
-
+                console.log(`\n users in ${JSON.stringify(users.listUsers())}`);
                 break;
-            case 'USER_LOGGED_OFF':
+            case `USER_LOGGED_OFF`:
                 console.log(`user logged off!`);
                 break;
-            case 'SEND_KEY_PRESS':
+
+            // Fire keypress event when admin presses keys in lightboard.
+            case `SEND_KEY_PRESS`:
                 broadcast({
-                    type: 'SEND_KEY_PRESS',
+                    type: `SEND_KEY_PRESS`,
                     key: data.key
                 }, ws);
-
-                console.log('received: ' + data.key);
-
+                console.log(`received: ${data.key}`);
                 break;
-
             default:
                 break;
         }
     });
 
-    ws.on('close', () => {
+    ws.on(`close`, () => {
         console.log(`user logged off ${userId}`);
 
+        // When user logs off, remove the user with their user ID from the list.
         if (userId) {
             users.removeUser(userId);
         }
 
         broadcast({
-            type: 'LIST_USERS',
+            type: `LIST_USERS`,
             users: users.listUsers()
         }, ws);
 
